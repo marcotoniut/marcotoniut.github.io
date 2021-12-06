449d9005cb83b77a01c697e8a3105cb16ba1bec6
Marco Toniut <marco.toniut@gmail.com>
2021-12-05 22:31:03 -0300

    Add CV page

---
+++ info.ts
@@ -0,0 +1,2 @@
+export const phone = "+44 7931 421697";
+export const email = "marco.toniut@gmail.com";
---
+++ styles/theme.ts
@@ -0,0 +1,29 @@
+export const colors = {
+  main: "#3552a1",
+  action: "#595d6a",
+  // action: "#5f7fe1",
+  aside: "#f4f5f7",
+  text: "#595d6a",
+} as const;
+
+export const fontSizes = {
+  xs: ".75rem",
+  small: ".8rem",
+  default: "1rem",
+  large: "1.25rem",
+  xl: "1.5rem",
+  xxl: "2rem",
+  xxxl: "2.5rem",
+} as const;
+
+export const space = {
+  none: "0rem",
+  xxs: ".3rem",
+  xs: ".5rem",
+  small: ".7rem",
+  default: "1rem",
+  large: "1.2rem",
+  xl: "1.6rem",
+  xxl: "2rem",
+  xxxl: "2.5rem",
+} as const;
