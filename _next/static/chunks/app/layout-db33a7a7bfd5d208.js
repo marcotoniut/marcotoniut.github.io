b1dfdf9c0a09fdd44c4771b1109cfb4f6c6a54d4
Marco Toniut <marco.toniut@gmail.com>
2025-10-12 22:20:58 +0100

    Updates

---
+++ _next/static/chunks/app/layout-db33a7a7bfd5d208.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1860:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6749,23))},6749:()=>{}},e=>{e.O(0,[642,372,414,358],()=>e(e.s=1860)),_N_E=e.O()}]);
